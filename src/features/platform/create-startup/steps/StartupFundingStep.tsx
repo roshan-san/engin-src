2e8bbde9ad43ba374648f6e125aa052b0972abd5
roshan <roshanjeyarubanr@gmail.com>
2025-06-24 21:58:28 +0530

    linting

--- src/features/platform/create-startup/steps/StartupFundingStep.tsx
+++ src/features/platform/create-startup/steps/StartupFundingStep.tsx
@@ -2,8 +2,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { useCreateStartup } from "../context/CreateStartupContext";
-import { useNavigate } from "@tanstack/react-router";
-
 export const StartupFundingStep = () => {
   const {
     startupData,
