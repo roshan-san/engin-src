1deca303dd4536871a4e7891eb14f5b3c3fcfa77
roshanjeyarubanr <roshanjeyarubanr@gmail.com>
2025-07-20 21:03:31 +0530

    summa

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vite.dev/config/
@@ -11,56 +10,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({ 
-      registerType: 'prompt',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: "Engin",
-        short_name: "Engin",
-        description: "A platform for connecting entrepreneurs and startups",
-        theme_color: "#000000",
-        background_color: "#000000",
-        display: "standalone",
-        orientation: "portrait",
-        scope: "/",
-        start_url: "/",
-        icons: [
-          {
-            src: "/web-app-manifest-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable"
-          },
-          {
-            src: "/web-app-manifest-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable"
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
-              },
-              cacheKeyWillBeUsed: async ({ request }) => {
-                return `${request.url}?${Date.now()}`;
-              }
-            }
-          }
-        ]
-      }
-    }),
       TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
   ],
   resolve: {
